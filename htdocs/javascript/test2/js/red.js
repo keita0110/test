87fd4a4529902e7cb890458be3b4044379e4ba5e
keita.akibayashi <keita.akibayashi@epkotsoftware.co.jp>
2023-04-21 17:32:37 +0900

    ファイル移動

---
+++ htdocs/javascript/test2/js/red.js
@@ -0,0 +1,33 @@
+(() => {
+    var canvas = document.getElementById('canvasBall');
+    var ctx = canvas.getContext('2d');
+
+    var ball = {
+        x: 0,
+        y: 100,
+        vx: 10,
+        radius: 15,
+        color: 'red',
+        draw: function () {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fillStyle = this.color;
+            ctx.fill();
+        }
+    };
+
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ball.draw();
+        ball.x += ball.vx;
+
+        if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+            ball.vx = -ball.vx;
+        }
+
+        raf = window.requestAnimationFrame(draw);
+    }
+
+    raf = window.requestAnimationFrame(draw);
+})();
