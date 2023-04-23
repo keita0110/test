340e457178ed594d91a2a0b56c919628e44ab6cf
keita.akibayashi <keita.akibayashi@epkotsoftware.co.jp>
2023-04-23 22:10:58 +0900

    クリックアクション

---
+++ htdocs/javascript/test2/js/index.js
@@ -0,0 +1,14 @@
+// (() => {
+//     const buttonAddRedBall = document.getElementById('buttonAddRedBall');
+//     const buttonAddBlueBall = document.getElementById('buttonAddBlueBall');
+
+//     const canvasController = new CanvasController(canvasBall);
+
+//     buttonAddRedBall.addEventListener('click', event => {
+//         canvasController.AddRedBall();
+//     });
+
+//     buttonAddBlueBall.addEventListener('click', event => {
+//         canvasController.AddBlueBall();
+//     });
+// });
