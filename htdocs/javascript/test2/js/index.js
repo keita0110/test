cd8e2b1d0de04df3b321ab2e28c81d885fc5d64f
keita.akibayashi <keita.akibayashi@epkotsoftware.co.jp>
2023-04-27 09:19:25 +0900

    canvas 修正

--- htdocs/javascript/test2/js/index.js
+++ htdocs/javascript/test2/js/index.js
@@ -1,14 +1,40 @@
-// (() => {
-//     const buttonAddRedBall = document.getElementById('buttonAddRedBall');
-//     const buttonAddBlueBall = document.getElementById('buttonAddBlueBall');
+(() => {
+    const buttonAddRedBall = document.getElementById('buttonAddRedBall');
+    const buttonAddBlueBall = document.getElementById('buttonAddBlueBall');
 
-//     const canvasController = new CanvasController(canvasBall);
+    var canvas = document.getElementById('canvasBall');
+    var ctx = canvas.getContext('2d');
 
-//     buttonAddRedBall.addEventListener('click', event => {
-//         canvasController.AddRedBall();
-//     });
+    var ball = {
+        x: 0,
+        y: 100,
+        vx: 15,
+        radius: 25,
+        color: 'red',
+        draw: function () {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fillStyle = this.color;
+            ctx.fill();
+        }
+    };
+    buttonAddRedBall.addEventListener('click', (ev) => {
 
-//     buttonAddBlueBall.addEventListener('click', event => {
-//         canvasController.AddBlueBall();
-//     });
-// });
+        function draw() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ball.draw();
+            ball.x += ball.vx;
+
+            if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+                ball.vx = -ball.vx;
+            }
+
+            window.requestAnimationFrame(draw);
+        }
+        window.requestAnimationFrame(draw);
+
+    });
+    buttonAddBlueBall.addEventListener('click', event => {
+    });
+})();
